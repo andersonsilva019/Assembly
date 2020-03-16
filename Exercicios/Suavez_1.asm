7eb7e74c0544f43f8443b676f7ecb32aec00d336
Anderson0017 <andeerson0017@gmail.com>
2020-03-16 20:02:45 -0300

    Saída final do array -> 3 , 1 , 2

--- Exercicios/Suavez_1.asm
+++ Exercicios/Suavez_1.asm
@@ -1,7 +1,7 @@
 %include "io.inc"
 
 section .data
-arrayD DD 8,2,3
+arrayD DB 10h,20h,30h,      ;Definindo 1byte(equivale a 8 bits) 
 
 
 section .text
@@ -11,14 +11,17 @@ CMAIN:
     
     ;Copie o primeiro valor em EAX e troque EAX com o
     ;valor da segunda posição
-    MOV EAX,[arrayD+1]
-    XCHG EAX,[arrayD]
+    MOV AL,[arrayD]            ;copiando o primeiro valor do array
+    XCHG AL,[arrayD+2]         ;trocando os valores do EAX com arrayD+2
     
     ;Troque EAX com o terceiro valor do array e copie o
     ;valor de EAX para a primeira posição do array.
     
-    XCHG EAX,[arrayD]
-    MOV [arrayD],EAX
+    XCHG AL,[arrayD]           ;Trocando os valores de AL(parte menos significatica do register) com o valor da primeira posição do arrayD
+    MOV AH,[arrayD+1]          ;Movendo o valor da segunda posição para AH(parte mais significatica do register)
+    MOV [arrayD+1],AL          ;Movendo o valor de AL para a segunda posição do array
+    MOV [arrayD+2],AH          ;Movendo o valor de AH para a terceira posição do array
+    
                
             
     xor eax, eax
